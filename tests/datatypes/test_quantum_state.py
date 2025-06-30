ef323a65bd4a6e69dcc94d23d1f29f416a90fddd
Siavash Davani <siavash.davani@gmail.com>
2025-06-30 10:13:43 +0000

    test case: creation of the circuit

--- tests/datatypes/test_quantum_state.py
+++ tests/datatypes/test_quantum_state.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.typing as npt
+import qiskit
 from hypothesis import given
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
@@ -31,3 +32,25 @@ def test_QuantumState_using_nparrays(data: npt.NDArray[np.complex128]) -> None:
     assert np.allclose(state.wavefunction, data / np.linalg.norm(data))
     assert state.num_qubits == int(np.log2(len(data)))
     assert state.size == len(data)
+
+
+@given(
+    arrays(
+        dtype=np.complex128,
+        shape=st.sampled_from([4, 8, 16]),
+        elements=st.complex_numbers(
+            min_magnitude=0.01,
+            max_magnitude=1000,
+            allow_nan=False,
+            allow_infinity=False,
+        ),
+    ),
+)
+def test_QuantumState_mps_circuit(data: npt.NDArray[np.complex128]) -> None:
+    """Test the MPS circuit generation of QuantumState."""
+
+    # Create an instance of QuantumState
+    state = QuantumState.from_dense_data(data=data, normalize=True)
+
+    circuit = state.generate_mps_initializer_circuit(number_of_layers=1)
+    assert isinstance(circuit, qiskit.QuantumCircuit)
